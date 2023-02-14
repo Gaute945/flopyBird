619377e63713450a0f71192f492f9e03da6dbac2
MrBoobai <e-gaugro@iskule.no>
2023-02-14 11:57:21 +0100

    sprites prep

--- pygame.py
+++ pygame.py
@@ -4,24 +4,21 @@
 
 Todo:
 //bottom pipes
-sprites
-design sprites
-top pipes
-flosy bird charcther
+//design sprites
+//top pipes
+//flosy bird charcther
+//pipe
 flopy gravity
 pipe collision
 flopy collision
 collision detection
 sprites
-flosy bird
-pipe
-sky
-ground
-
 """
 
 import arcade
 import random
+
+"""defining"""
 
 SCREEN_WIDTH = 1920
 
@@ -30,6 +27,11 @@ SCREEN_TITLE = "Flosy"
 
 xlist = [40, 100, 120, 160, 320, 480, 640, 800, 960, 1120, 1280, 1440, 1600, 1760, 1920]
 hlist = [100, 200, 300, 400, 500]
+
+self.player_list = arcade.SpriteList(use_spatial_hash=True)
+self.wall_list = arcade.SpriteList()
+
+"""end of defining"""
 
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 arcade.set_background_color(arcade.color.BLACK)
@@ -43,6 +45,12 @@ for i in range(10):
     w = 100
     h = random.choice(hlist)
     arcade.draw_rectangle_filled(x, y, w, h, arcade.color.BRITISH_RACING_GREEN)
+    print(x, y)
+    
+while True:
+    if key == arcade.key.SPACE or key == arcade.key.W or key == arcade.key.UP:
+        if self.pysics_engine.can_jump():
+            self.playerSprite.change_y = playerJumpSpeed
 
 arcade.finish_render()
 arcade.run()
